cb82037e875e9f3ebc3e991a40839872885cbfea
Daniele Venier <danielevenier1998@gmail.com>
2019-06-27 16:34:44 +0100

    Various fixes

--- meet-me-app/src/Map.js
+++ meet-me-app/src/Map.js
@@ -2,63 +2,10 @@ import React from 'react';
 
 import mapboxgl from 'mapbox-gl';
 
-import MAP_STYLE from './basic-style.json';
-
 export default class Map extends React.Component {
     constructor(props){
         super(props);
-        let style = MAP_STYLE;
         this.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoic2FyYWhob3VjaGVpbWkiLCJhIjoiY2p4ZWp6dnY3MGNkNjN6czBrY2RuYml6ZyJ9.oN4O4hk4xXdx84KQRO7gSA';
-        style.sources["points-source"] = {
-            "type": "geojson",
-            "data": {
-                "type": "FeatureCollection",
-                "features": [
-                    {
-                        "type": "Feature",
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [55.863560, -4.260230]
-                        },
-                        "properties": {
-                            "name": "Rand stuff"
-                        }
-                    },
-                    {
-                        "type": "Feature",
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [55.861230, -4.282510]
-                        },
-                        "properties": {
-                            "name": "Rand stuff"
-                        }
-                    },
-                    {
-                        "type": "Feature",
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [55.873410, -4.282510]
-                        },
-                        "properties": {
-                            "name": "Rand stuff"
-                        }
-                    },
-                ]
-            }
-        }
-        style.layers.push({
-            "id": "points",
-            "type": "symbol",
-            "source": "points-source",
-            "layout": {
-                "icon-image": "favicon.ico",
-                "text-field": "{title}",
-                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-                "text-offset": [0, 0.6],
-                "text-anchor": "top"
-            }
-        });
         this.state = {
             places: [
                 {'lat': 55.861230, 'lng': -4.254540},
@@ -73,6 +20,28 @@ export default class Map extends React.Component {
         };
     }
 
+     toGeoJSON = (places_list) => {
+        var geojson = {
+            "type": "FeatureCollection",
+            "features": []
+        }
+
+        for(var i=0;i<places_list.length;i++){
+            geojson.features.push({
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [places_list[i].lng, places_list[i].lat]
+                },
+                "properties": {
+                    "name": "Rand stuff"
+                }
+            })
+        }
+
+        return geojson;
+    }
+
     componentDidMount(){
         console.log("MOUNT");
         mapboxgl.accessToken = 'pk.eyJ1Ijoic2FyYWhob3VjaGVpbWkiLCJhIjoiY2p4ZWp6dnY3MGNkNjN6czBrY2RuYml6ZyJ9.oN4O4hk4xXdx84KQRO7gSA';
@@ -81,61 +50,13 @@ export default class Map extends React.Component {
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [-4.26164, 55.86043],
             zoom: 16,
-<<<<<<< HEAD
         });
-=======
-        },
->>>>>>> 81dbd9195da6d9e23047e46a69473b74bd40cdee
 
 
         this.map.on('load', () => {
             this.map.addSource('places-data', {
                 "type": "geojson",
-                "data": {
-                    "type": "FeatureCollection",
-                    "features": [
-                        {
-                            "type": "Feature",
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [-4.26164, 55.86043]
-                            },
-                            "properties": {
-                                "name": "Rand stuff"
-                            }
-                        },
-                        {
-                            "type": "Feature",
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [-4.260230, 55.863560]
-                            },
-                            "properties": {
-                                "name": "Rand stuff"
-                            }
-                        },
-                        {
-                            "type": "Feature",
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [-4.282510, 55.861230]
-                            },
-                            "properties": {
-                                "name": "Rand stuff"
-                            }
-                        },
-                        {
-                            "type": "Feature",
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [-4.282510, 55.873410]
-                            },
-                            "properties": {
-                                "name": "Rand stuff"
-                            }
-                        },
-                    ]
-                }
+                "data": this.toGeoJSON(this.state.places)
             });
 
             this.map.loadImage('/pin.png', (error, image) => {
