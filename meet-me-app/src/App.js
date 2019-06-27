906288288bf8a1f1502a55d573f45aab45e44252
stuart <stuartslattery2@gmail.com>
2019-06-27 12:51:52 +0100

    Added basic map. Like really basic.

--- meet-me-app/src/App.js
+++ meet-me-app/src/App.js
@@ -30,6 +30,7 @@ class Map extends Component {
         return (
             <ReactMapGL
                 {...this.state.viewport}
+                mapboxApiAccessToken = {'pk.eyJ1Ijoic2FyYWhob3VjaGVpbWkiLCJhIjoiY2p4ZWp6dnY3MGNkNjN6czBrY2RuYml6ZyJ9.oN4O4hk4xXdx84KQRO7gSA'}
                 onViewportChange={(viewport) => this.setState({viewport})}
             />
         );
