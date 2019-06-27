bf5e2e992246ee1fbb326f0b0a502eae7d6ff25f
Maria Stefanova <mi47@st-andrews.ac.uk>
2019-06-27 16:26:19 +0100

    Added a new image for the navbar button

--- meet-me-app/src/App.js
+++ meet-me-app/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import {Component} from 'react';
 import Map from './Map.js';
+import SideNavPage from './sidebar.js'
 
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
   let map = new Map();
 
   return (
+    <SideNavPage  />,
     map.render()
   );
 }
